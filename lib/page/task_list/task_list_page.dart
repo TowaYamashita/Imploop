28ae5c1a1b3f03e32e77dc9a237e2f6ac585f991
TowaYamashita <Confirm.Apps.Develop@gmail.com>
2021-12-13 19:49:51 +0900

    Task新規追加画面の追加

---
+++ lib/page/task_list/task_list_page.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:sample/domain/task.dart';
+import 'package:sample/service/task_service.dart';
+
+class TaskListPage extends StatelessWidget {
+  const TaskListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Taskリスト'),
+      ),
+      body: const _TaskList(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return const _TaskCreatePage();
+              },
+              fullscreenDialog: true,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _TaskList extends StatelessWidget {
+  const _TaskList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Task>>(
+      future: TaskService.getAllTask(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        final List<Task>? _taskList = snapshot.data ?? [];
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(
+                _taskList![index].name.toString(),
+              ),
+              subtitle: Text(
+                'taskId: ${_taskList[index].taskId} statusId: ${_taskList[index].statusId}',
+              ),
+            );
+          },
+          itemCount: _taskList!.length,
+        );
+      },
+    );
+  }
+}
+
+class _TaskCreatePage extends StatelessWidget {
+  const _TaskCreatePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Task入力'),
+      ),
+      body: Center(
+        child: Form(
+          child: Column(
+            children: [
+              TextField(
+                onSubmitted: (value) async {
+                  final Task addedTask =
+                      await TaskService.registerNewTask(value);
+                  // Taskが追加されたことをスナックバーで通知
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        'Taskが追加されました。',
+                      ),
+                    ),
+                  );
+                  // 前の画面に遷移
+                  Navigator.pop(context);
+                },
+              ),
+              ElevatedButton(
+                onPressed: () => {},
+                child: const Text('登録する'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
