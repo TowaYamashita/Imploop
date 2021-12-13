8676ff9ee30cb0e83f179f05ead0762b95654a15
TowaYamashita <Confirm.Apps.Develop@gmail.com>
2021-12-13 19:49:10 +0900

    ホーム画面からTask一覧画面へ遷移できるボタンを追加

---
+++ lib/page/home/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:sample/page/task_list/task_list_page.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ホーム'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) {
+                  return const TaskListPage();
+                },
+              ),
+            )
+          },
+          child: const Text('Task・Todo入力画面へ遷移'),
+        ),
+      ),
+    );
+  }
+}
